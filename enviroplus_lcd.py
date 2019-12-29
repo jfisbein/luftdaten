456c80039e563ff808583abad59a46170ff56c5d
Joan Fisbein <joan@fisbein.com>
2019-12-29 16:13:05 +0100

    Trying to modularize the code

---
+++ enviroplus_lcd.py
@@ -0,0 +1,43 @@
+import datetime
+
+import ST7735
+from PIL import Image, ImageDraw
+
+
+class EnviroplusLCD:
+    def __init__(self, font):
+        # Create LCD instance
+        self.disp = ST7735.ST7735(
+            port=0,
+            cs=ST7735.BG_SPI_CS_FRONT,
+            dc=9,
+            backlight=12,
+            rotation=270,
+            spi_speed_hz=10000000
+        )
+        # Initialize display
+        self.disp.begin()
+
+        self.WIDTH = self.disp.width
+        self.HEIGHT = self.disp.height
+        self.font = font
+
+    # Display Last request date, last request status and Wi-Fi status on LCD
+    def display_status(self, wifi_status, info_status):
+        # wifi_status = "connected" if check_wifi() else "disconnected"
+        text_colour = (255, 255, 255)
+        ok_back_colour = (0, 0, 0)  # Black
+        error_back_colour = (85, 15, 15)  # Red
+        back_colour = ok_back_colour if wifi_status == 'connected' else error_back_colour
+        message = "{:%Y-%m-%d %H:%M:%S}\nLast update: {}\nWi-Fi: {}".format(datetime.datetime.now(), info_status, wifi_status)
+        img = Image.new('RGB', (self.WIDTH, self.HEIGHT), color=(0, 0, 0))
+        draw = ImageDraw.Draw(img)
+        size_x, size_y = draw.textsize(message, self.font)
+        x = (self.WIDTH - size_x) / 2
+        y = (self.HEIGHT / 2) - (size_y / 2)
+        draw.rectangle((0, 0, 160, 80), back_colour)
+        draw.text((x, y), message, font=self.font, fill=text_colour)
+        self.disp.display(img)
+
+    def turnoff(self):
+        self.disp.set_backlight(0)
---
+++ enviroplus_reader.py
@@ -0,0 +1,61 @@
+import time
+from subprocess import PIPE, Popen
+
+from bme280 import BME280
+from enviroplus import gas
+from pms5003 import PMS5003, ReadTimeoutError
+from smbus2 import SMBus
+
+try:
+    # Transitional fix for breaking change in LTR559
+    from ltr559 import LTR559
+
+    ltr559 = LTR559()
+except ImportError:
+    import ltr559
+
+
+class EnviroPlusReader:
+    def __init__(self, comp_factor: float = 1.2) -> object:
+        self.bus = SMBus(1)
+
+        # Create BME280 (temp, humidity and pressure sensor) instance
+        self.bme280 = BME280(i2c_dev=self.bus)
+
+        # Create PMS5003 dust sensor instance
+        self.pms5003 = PMS5003()
+        self.comp_factor = comp_factor
+
+    def read_values(self):
+        values = {}
+        cpu_temp = self.get_cpu_temperature()
+        raw_temp = self.bme280.get_temperature()
+        comp_temp = raw_temp - ((cpu_temp - raw_temp) / self.comp_factor)
+        values['temperature'] = round(comp_temp, 2)
+        values['pressure'] = round(self.bme280.get_pressure(), 2)
+        values['humidity'] = round(self.bme280.get_humidity(), 2)
+        data = gas.read_all()
+        values['oxidising'] = round(data.oxidising / 1000, 4)
+        values['reducing'] = round(data.reducing / 1000, 4)
+        values['nh3'] = round(data.nh3 / 1000, 4)
+        values['lux'] = round(ltr559.get_lux(), 2)
+        try:
+            pm_values = self.pms5003.read()
+            values['P2.5'] = pm_values.pm_ug_per_m3(2.5)
+            values['P10'] = pm_values.pm_ug_per_m3(10)
+            values['P1.0'] = pm_values.pm_ug_per_m3(1.0)
+        except ReadTimeoutError:
+            self.pms5003.reset()
+            pm_values = self.pms5003.read()
+            values['P2.5'] = pm_values.pm_ug_per_m3(2.5)
+            values['P10'] = pm_values.pm_ug_per_m3(10)
+            values['P1.0'] = pm_values.pm_ug_per_m3(1.0)
+
+        values['ts'] = time.time_ns()
+        return values
+
+    @staticmethod
+    def get_cpu_temperature():
+        process = Popen(['vcgencmd', 'measure_temp'], stdout=PIPE, universal_newlines=True)
+        output, _error = process.communicate()
+        return float(output[output.index('=') + 1:output.rindex("'")])
