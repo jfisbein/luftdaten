7fa87ce355b284b9fc1ae64e434a00e02be7e7ec
Joan Fisbein <joan@fisbein.com>
2019-12-29 16:12:39 +0100

    Trying to modularize the code

---
+++ influxdb_weather_client.py
@@ -0,0 +1,32 @@
+from influxdb_client import InfluxDBClient
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+
+class InfluxDbWeather:
+    def __init__(self, url: str, bucket_id: str, org: str, token: str, buffer_size: int = 100) -> object:
+        self.bucket_id = bucket_id
+        self.org = org
+        self.values_buffer = []
+        self.client = InfluxDBClient(url=url, token=token, org=org, enable_gzip=True)
+        self.write_client = self.client.write_api(write_options=SYNCHRONOUS)
+        self.buffer_size = buffer_size
+
+    @staticmethod
+    def map_to_influxdb(values):
+        influxdb_messages = []
+        for value in values:
+            influxdb_messages.append("weather,location=acacias temperature={},humidity={},pressure={} {}"
+                                     .format(value['temperature'], value['humidity'], value['pressure'], value['ts']))
+            influxdb_messages.append("particles,location=acacias P25={},P10={},P1={} {}"
+                                     .format(value['P2.5'], value['P10'], value['P1.0'], value['ts']))
+            influxdb_messages.append("gas,location=acacias oxidising={},reducing={},nh3={} {}"
+                                     .format(value['oxidising'], value['reducing'], value['nh3'], value['ts']))
+
+        return influxdb_messages
+
+    def send_to_influxdb(self, values):
+        self.values_buffer.append(values)
+        if len(self.values_buffer) >= self.buffer_size:
+            # logger.info("Sending data to influxDB")
+            self.write_client.write(self.bucket_id, self.org, self.map_to_influxdb(self.values_buffer))
+            self.values_buffer = []
---
+++ luftdaten_client.py
@@ -0,0 +1,45 @@
+import requests
+
+
+class LuftdatenClient:
+    def __init__(self, sensor_id: str) -> object:
+        self.sensor_id = sensor_id
+
+    def send_to_luftdaten(self, values):
+        resp_1 = requests.post("https://api.luftdaten.info/v1/push-sensor-data/",
+                               json={
+                                   "software_version": "enviro-plus 0.0.1",
+                                   "sensordatavalues": [
+                                       {'value_type': 'P1', 'value': str(values['P10'])},
+                                       {'value_type': 'P2', 'value': str(values['P2.5'])}
+                                   ]
+                               },
+                               headers={
+                                   "X-PIN": "1",
+                                   "X-Sensor": self.sensor_id,
+                                   "Content-Type": "application/json",
+                                   "cache-control": "no-cache"
+                               }
+                               )
+
+        resp_2 = requests.post("https://api.luftdaten.info/v1/push-sensor-data/",
+                               json={
+                                   'software_version': '"enviro-plus 0.0.1',
+                                   'sensordatavalues': [
+                                       {'value_type': 'temperature', 'value': str(values['temperature'])},
+                                       {'value_type': 'humidity', 'value': str(values['humidity'])},
+                                       {'value_type': 'pressure', 'value': str(values['pressure'])}
+                                   ]
+                               },
+                               headers={
+                                   "X-PIN": "11",
+                                   "X-Sensor": self.sensor_id,
+                                   "Content-Type": "application/json",
+                                   "cache-control": "no-cache"
+                               }
+                               )
+
+        if resp_1.ok and resp_2.ok:
+            return True
+        else:
+            return False
